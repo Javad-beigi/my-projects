d146ad1bf24e01cc07e4e33c805a5cc235791467
javad-beigi <javad.beigi97@gmail.com>
2024-01-17 04:37:17 +0330

    wordcloud project

---
+++ WORDCLOUD-MOVIES/README.md
@@ -0,0 +1,19 @@
+# Wordcloud Generator
+This is a simple wordcloud generator. It takes a text file as input and generates a wordcloud image as output.
+
+## Usage
+### 1. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+
+### 2. Run the script
+To run the script, first cd into the root directory of the project and then run the following command:
+```bash
+export PYTHONPATH=$(pwd)
+```
+
+Then run the following command:
+```bash
+python wordcloud.py <input_file> <output_file>
+```
---
+++ WORDCLOUD-MOVIES/src/run.py
@@ -0,0 +1,28 @@
+from wordcloud import WordCloud
+
+
+class WordCloudGenerator:
+    def __init__(self, file_path: str):
+        with open(file_path) as f:
+            self.text = f.read()
+
+    def run(self, output_path: str, **kwargs) -> None:
+        """
+        Generate a word cloud image.
+
+        :param output_path: The path to the output file.
+        """
+        WordCloud(**kwargs).generate(self.text).to_file(output_path)
+
+
+if __name__ == '__main__':
+    import sys
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    wc_gen = WordCloudGenerator(input_file)
+    wc_gen.run(
+        output_file,
+        width=600, height=400,
+        background_color='white',
+    )
